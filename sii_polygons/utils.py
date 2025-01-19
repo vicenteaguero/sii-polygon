ffbbdaa1e48c731d1e67944b69bf3c74562724bd
Vicente Agüero <vicenteaguero@uc.cl>
2025-01-19 20:19:31 -0300

    fix(polygons): :bug: Fix how save folder is computed (TODO: add services)

--- sii_polygons/utils.py
+++ sii_polygons/utils.py
@@ -10,6 +10,7 @@ def get_bbox(xmin: float, xmax: float, ymin: float, ymax: float) -> str:
 def get_coords(bbox: str) -> tuple[float, float, float, float]:
     return tuple(map(float, bbox.split(',')))
 
-def get_save_map_folder(comuna_id: int, xmin: float) -> str:
+def get_save_map_folder(comuna: str, size: int, xmin: float) -> str:
+    size_folder = f'{size}x{size}'
     xmin_folder = int(xmin)//100
-    return os.path.join(PATHS['predios'], f'{comuna_id}', f'{xmin_folder}')
+    return os.path.join(PATHS['predios'], comuna.upper(), size_folder, f'{xmin_folder}')
