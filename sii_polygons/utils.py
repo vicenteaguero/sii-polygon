c16b41242fcb63e74efdff2489ed84382f0ac481
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-02 02:33:54 -0300

    feat(polygons): :sparkles: Added services to method for generating folder names

--- sii_polygons/utils.py
+++ sii_polygons/utils.py
@@ -10,7 +10,9 @@ def get_bbox(xmin: float, xmax: float, ymin: float, ymax: float) -> str:
 def get_coords(bbox: str) -> tuple[float, float, float, float]:
     return tuple(map(float, bbox.split(',')))
 
-def get_save_map_folder(comuna: str, size: int, xmin: float) -> str:
+def get_save_map_folder(comuna: str, service: str, size: int, xmin: float) -> str:
     size_folder = f'{size}x{size}'
     xmin_folder = int(xmin)//100
-    return os.path.join(PATHS['predios'], comuna.upper(), size_folder, f'{xmin_folder}')
+    if service not in PATHS:
+        raise NotImplementedError(f'Service {service} not implemented')
+    return os.path.join(PATHS[service], comuna.upper(), size_folder, f'{xmin_folder}')
